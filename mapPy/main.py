ee7601d404fa53ed78fa18820f9d3d4a4049b8cc
th3-k3rnl <aaron.ilift@gmail.com>
2023-09-09 16:22:22 +0100

    Adding custom icon and window caption

--- mapPy/main.py
+++ mapPy/main.py
@@ -3,28 +3,29 @@ import pygame
 
 WINDOW_WIDTH = 1280
 WINDOW_HEIGHT = 720
+GAME_TITLE = "mapPy"
 
 # pygame setup
 pygame.init()
+pygame.display.set_caption(f'Welcome to {GAME_TITLE}')
+icon = pygame.image.load("assets/intro_ball.gif")
+pygame.display.set_icon(icon)
 screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+
 clock = pygame.time.Clock()
 running = True
 
 while running:
-    # poll for events
-    # pygame.QUIT event means the user clicked X to close your window
+    # Event loop
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
-    # fill the screen with a color to wipe away anything from last frame
+    # Window visual render happens here
     screen.fill("white")
     map = pygame.image.load("assets/world-map.png")
     scaledMap = pygame.transform.scale(map, (WINDOW_WIDTH, WINDOW_HEIGHT))
     screen.blit(scaledMap, (0, 0))
-    # RENDER YOUR GAME HERE
-
-    # flip() the display to put your work on screen
     pygame.display.flip()
 
     clock.tick(60)  # limits FPS to 60
